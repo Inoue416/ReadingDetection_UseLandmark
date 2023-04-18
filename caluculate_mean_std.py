9789f17c477541b5df5b904f47700d4de6a594ce
yuyainoue <yuyatuta@icloud.com>
2023-04-18 14:18:49 +0900

    np配列にする際に配列の構造が違うという警告が出たため、x、yともに一列の配列にして結合、後にnp配列にして対処

--- caluculate_mean_std.py
+++ caluculate_mean_std.py
@@ -41,8 +41,8 @@ if __name__ == '__main__':
     bar = '#' * progress + " " * (10-progress)
     for train_path in train_paths:
         data = load_data(train_path)
-        train_x.append(deepcopy(data[:, :, 0]).sum())
-        train_y.append(deepcopy(data[:, :, 1]))
+        train_x += deepcopy(data[:, :, 0]).reshape(-1).tolist()
+        train_y += deepcopy(data[:, :, 1]).reshape(-1).tolist()
         count += 1
         print(f"\r\033[K[{bar}] {progress/10*100:.02f}% ({progress}/{10})", end="")
         if count % step == 0:
@@ -56,13 +56,39 @@ if __name__ == '__main__':
     bar = '#' * progress + " " * (10-progress)
     for val_path in val_paths:
         data = load_data(val_path)
-        val_x.append(deepcopy(data[:, :, 0]))
-        val_y.append(deepcopy(data[:, :, 1]))
+        val_x += deepcopy(data[:, :, 0]).reshape(-1).tolist()
+        val_y += deepcopy(data[:, :, 1]).reshape(-1).tolist()
         count += 1
         print(f"\r\033[K[{bar}] {progress/10*100:.02f}% ({progress}/{10})", end="")
         if count % step == 0:
             progress += 1
             bar = '#' * progress + " " * (10-progress)
     print()
+    total_x = []
+    total_x += deepcopy(train_x)
+    total_x += deepcopy(val_x)
+    total_y = []
+    total_y += deepcopy(train_y)
+    total_y += deepcopy(val_y)
+
     train_x = np.array(train_x)
-    print(np.mean(train_x))
+    train_y = np.array(train_y)
+    val_x = np.array(val_x)
+    val_y = np.array(val_y)
+    total_x = np.array(total_x)
+    total_y = np.array(total_y)
+
+    print('train x mean: ', train_x.mean())
+    print('train y mean: ', train_y.mean())
+    print('\ntrain x std: ', train_x.std(ddof=1))
+    print('train y std: ', train_y.std(ddof=1))
+
+    print("\nval x mean: ", val_x.mean())
+    print('val y mean: ', val_y.mean())
+    print('\nval x std: ', val_x.std(ddof=1))
+    print('val y std: ', val_y.std(ddof=1))
+
+    print('\ntotal x mean: ', total_x.mean())
+    print('total y mean: ', total_y.mean())
+    print('\ntotal x std: ', total_x.std(ddof=1))
+    print('total y std: ', total_y.std(ddof=1))
